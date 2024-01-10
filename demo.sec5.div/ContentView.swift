3d38991323f0790dd85e64e2b3c765c77d87fb34
div <divpatel27@gmail.com>
2024-01-10 01:25:05 -0500

    Initial Commit

---
+++ demo.sec5.div/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  demo.sec5.div
+//
+//  Created by user237065 on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demo.sec5.div/demo_sec5_divApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_sec5_divApp.swift
+//  demo.sec5.div
+//
+//  Created by user237065 on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct demo_sec5_divApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
